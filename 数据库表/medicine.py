1388346e6e2680a7ef8dd15b66d4ab6e89e6464f
unknown <wydewy@126.com>
2017-09-29 13:03:29 +0800

    modify

--- 数据库表/medicine.py
+++ 数据库表/medicine.py
@@ -1,5 +1,5 @@
 from xlsx2sql import XlsxTool,Xlsx2sql
 tool = XlsxTool()
-table_header_type = ['varchar(255) not null','varchar(255) ','varchar(255)','varchar(255) ','varchar(255)','varchar(255)','varchar(255)','varchar(255)','varchar(255) ','date','varchar(255)','varchar(255) ','varchar(255) ']
+table_header_type = ['varchar(255) not null','varchar(255) ','varchar(255)','varchar(255) ','varchar(255)','varchar(255)','varchar(255)','varchar(255) ','varchar(255)','varchar(255)','varchar(255) ','date','date','varchar(255)','varchar(255) ','varchar(255) ','varchar(255) ','varchar(255) ']
 release = Xlsx2sql(tool)
 release.generate(r'./medicine.xlsx',table_header_type,"id",r'./medicine.sql')
